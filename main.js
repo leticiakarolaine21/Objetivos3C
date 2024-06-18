23aac2a7e4a31a43a8c925279d8ad5cf8d5adbcf
leticiakarolaine21 <145347965+leticiakarolaine21@users.noreply.github.com>
2024-06-18 09:56:40 -0300

    --5

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for(let i=0;i  <botoes.length;i++){
     }
     const contadores = document.querySelectorAll(“.contador”);
     const tempoObjetivo1 = new Date(“2024-10-05T00:00:00”);
-    contadores[0].textContent = calculatempo tempoObjetivo1;
+    contadores[0].textContent = calculaTEMPO tempoObjetivo1;
     function calculaTempo(tempoObjetivo) {
         let tempoAtual = new Date();
         let tempoFinal = tempoObjetivo - tempoAtual;
